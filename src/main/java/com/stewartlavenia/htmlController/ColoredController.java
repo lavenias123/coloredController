1860a87f7398cd2ea4e9a210758ebfa259d6ae7b
Lavenia Stewart <lavenias123@gmail.com>
2024-05-02 12:41:57 -0700

    don't know how to create records in controller

--- src/main/java/com/stewartlavenia/htmlController/ColoredController.java
+++ src/main/java/com/stewartlavenia/htmlController/ColoredController.java
@@ -1,13 +1,39 @@
 package com.stewartlavenia.htmlController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ColoredController {
 
 	@GetMapping("yellow")
-	public String yellow() {
+	public String yellow(Model model) {
+		System.out.println("Getting all users!");
+		List<Users> users = new ArrayList<>();
+//		List<Users> users = new Users();
+		
+		model.addAttribute("one", 1);
+		model.addAttribute("two", 2);
+		
+		model.addAttribute("us", users);
+		
+//		model.addAllAttributes(users);
+		
+//		new Users("Bobby Brown", "1234", 22);
+//		model.addAttribute("Bobby Brown", "1234", 22);
+//		users.add[new Users(name:"Bobby Brown", "1234", 22)];
+//		users.addAll(users);
+		
+//		model.addAttribute("Bobby Brown", "1234", 22);
+//		users.add("Michael Jackson", "1345", 12);
+//		users.add("Sammy Davis Jr.", "2445", 35);
+//		users.add("Denise Williams", "4444", 55);
+//		
+		
 		return "yellow";
 	}
 	
---
+++ src/main/java/com/stewartlavenia/htmlController/Users.java
@@ -0,0 +1,29 @@
+package com.stewartlavenia.htmlController;
+
+public class Users {
+	
+	private String name;
+	private String pwd;
+	private int size;
+	public Users(String string, String string2, int i) {
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+}
